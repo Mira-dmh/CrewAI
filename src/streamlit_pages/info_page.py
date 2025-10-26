7fb44f89f79a88c86b3b25fb73e38a82a4ac64ec
rowflynn <rflynn@arcadia.edu>
2025-10-26 17:18:08 -0400

    small changes to info dashboard

--- src/streamlit_pages/info_page.py
+++ src/streamlit_pages/info_page.py
@@ -18,17 +18,21 @@ col4, col5, col6 = st.columns(3)
 
 # 1. Job Market Overview
 with col1:
-    st.header("1. Job Market Overview")
+    st.header("Job Description")
     st.write(data.get("job_description", "No data available."))
 
 # 2. Top Hiring Companies
 with col2:
-    st.header("2. Top Hiring Companies")
-    st.write("hiring_trends", "No data available.")
+    st.header("Market Trends")
+    if data["hiring_trends"]:
+        for trend in data["hiring_trends"]:
+            st.markdown(f"- {trend}")
+    else:
+        st.write("No data available.")
 
 # 3. In-Demand Skills & Tools
 with col3:
-    st.header("3. In-Demand Skills & Tools")
+    st.header("Top Hiring Companies")
     if data["top_hiring_companies"]:
         for company in data["top_hiring_companies"]:
             st.markdown(f"- {company}")
@@ -37,7 +41,7 @@ with col3:
 
 # 4. Salary Range & Locations
 with col4:
-    st.header("4. Salary Range & Locations")
+    st.header("Required Skills")
     if data["required_skills"]:
         for skill in data["required_skills"]:
             st.markdown(f"- {skill}")
@@ -46,9 +50,9 @@ with col4:
 
 # 5. Interview Topics / Skill Focus
 with col5:
-    st.header("5. Interview Topics / Skill Focus")
+    st.header("Placeholder")
     
 
 # 6. Citations or Source URLs
 with col6:
-    st.header("6. Citations / Source URLs")
+    st.header("Placeholder")
